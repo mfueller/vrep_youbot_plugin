afc9184d4f581abb4889a61844d58ccb55df9c0a
Matthias Fueller <matthias.fueller@h-brs.de>
2013-05-16 17:09:07 +0200

    small changes

--- test/test_joint_position.sh
+++ test/test_joint_position.sh
@@ -1,6 +1,9 @@
-rostopic pub -1 /arm_controller/position_command  brics_actuator/JointPositions '{positions: [{joint_uri: arm_joint_1, unit: rad, value: 1.5}]}'
-rostopic pub -1 /arm_controller/position_command  brics_actuator/JointPositions '{positions: [{joint_uri: arm_joint_2, unit: rad, value: 1.5}]}'
-rostopic pub -1 /arm_controller/position_command  brics_actuator/JointPositions '{positions: [{joint_uri: arm_joint_3, unit: rad, value: -1.5}]}'
-rostopic pub -1 /arm_controller/position_command  brics_actuator/JointPositions '{positions: [{joint_uri: arm_joint_4, unit: rad, value: 1.5}]}'
-rostopic pub -1 /arm_controller/position_command  brics_actuator/JointPositions '{positions: [{joint_uri: arm_joint_5, unit: rad, value: 1.5}]}'
+rostopic pub -1 /arm_controller/position_command  brics_actuator/JointPositions '{positions: [
+{joint_uri: arm_joint_1, unit: rad, value: 2.9},
+{joint_uri: arm_joint_2, unit: rad, value: 1.1},
+{joint_uri: arm_joint_3, unit: rad, value: -2.5},
+{joint_uri: arm_joint_4, unit: rad, value: 1.7},
+{joint_uri: arm_joint_5, unit: rad, value: 1.5}
+]}' 
+
 
