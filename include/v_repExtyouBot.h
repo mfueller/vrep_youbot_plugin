33c72d434f024a2c8572e83a4001b817f6ffcffa
Argentina Ortega Sainz <argentina.ortega@smail.inf.h-brs.de>
2016-03-16 02:01:45 +0100

    Updated files for v-rep version 3.3.0

--- include/v_repExtyouBot.h
+++ include/v_repExtyouBot.h
@@ -1,28 +1,27 @@
-// Copyright 2006-2013 Dr. Marc Andreas Freese. All rights reserved. 
+// Copyright 2006-2016 Coppelia Robotics GmbH. All rights reserved.
 // marc@coppeliarobotics.com
 // www.coppeliarobotics.com
-// 
+//
 // -------------------------------------------------------------------
-// This file is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// 
+// THIS FILE IS DISTRIBUTED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+// WARRANTY. THE USER WILL USE IT AT HIS/HER OWN RISK. THE ORIGINAL
+// AUTHORS AND COPPELIA ROBOTICS GMBH WILL NOT BE LIABLE FOR DATA LOSS,
+// DAMAGES, LOSS OF PROFITS OR ANY OTHER KIND OF LOSS WHILE USING OR
+// MISUSING THIS SOFTWARE.
+//
 // You are free to use/modify/distribute this file for whatever purpose!
 // -------------------------------------------------------------------
 //
-// This file was automatically created for V-REP release V3.0.1 on January 20th 2013
+// This file was automatically created for V-REP release V3.3.0 on February 19th 2016
 
-#pragma once
+#ifndef V_REPYOUBOT_H
+#define V_REPYOUBOT_H
 
-#ifdef _WIN32
-	#define VREP_DLLEXPORT extern "C" __declspec(dllexport)
-#endif /* _WIN32 */
-#if defined (__linux) || defined (__APPLE__)
-	#define VREP_DLLEXPORT extern "C"
-#endif /* __linux || __APPLE__ */
-
+#define VREP_DLLEXPORT extern "C"
 
 // The 3 required entry points of the V-REP plugin:
 VREP_DLLEXPORT unsigned char v_repStart(void* reservedPointer,int reservedInt);
 VREP_DLLEXPORT void v_repEnd();
 VREP_DLLEXPORT void* v_repMessage(int message,int* auxiliaryData,void* customData,int* replyData);
+
+#endif
